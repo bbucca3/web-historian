a7c4b80712ef11172912a0fa310113bf26c00a2c
Hack Reactor Student <student@hackreactor.com>
2017-06-10 17:33:55 -0700

    working on request handler helpers

---
+++ helpers/handleRequestJournal.md
@@ -0,0 +1,14 @@
+1. Specification:
+Input is an array. Output is nothing. Side effects include creating files in the archive folder.
+
+2. Justification:
+To download the html files.
+
+3. Explanation:
+This function will download web pages and store them as files in the archive folder.
+
+4. Visualization:
+See image.
+
+
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,49 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
 };
+
+exports.handleRequest = function (req, res) {
+
+
+  console.log(req.url);
+
+//   var statusCode = 200;
+
+//  (method === 'POST') {
+//  +
+//  +    console.log('URL: ', request.url);
+//  +   
+
+//   response.writeHead(statusCode, headers);
+
+// if (request.method === 'POST') {
+//  +    var body = '';
+//  +
+//  +    request.on('data', function(data) {
+//  +      body += data;
+//  +    });
+  
+//  -  if (request._postData !== undefined) {
+//  -    messages.results.push(request._postData);
+//  +    request.on('end', function() {
+//  +      var messageObj = JSON.parse(body);
+//  +      messageObj.objectId = messages.results.length + 1;
+//  +      messages.results.push(messageObj);
+//  +    });
+//     }
+  
+//     var messagesJSON = JSON.stringify(messages);
+//  +  console.log('messagesJSON ', messagesJSON);
+//     response.end(messagesJSON);
+
+  
+  //res.end(archive.paths.list);
+};
+
